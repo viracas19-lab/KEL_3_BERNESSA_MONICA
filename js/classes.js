b812c5b8831fe32b04681e2fc44aa343040dea44
viracas19-lab <viracas19@gmail.com>
2026-02-11 09:05:28 +0700

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -5,10 +5,10 @@ let filteredData = [];
 
 // FETCH & JOIN DATA
 Promise.all([
-  fetch('./data/classes_rows.json').then(r => r.json()),
+  fetch('./data/class_rows.json').then(r => r.json()),
   fetch('./data/lesson_schedule_rows.json').then(r => r.json()),
   fetch('./data/teachers_rows.json').then(r => r.json()),
-  fetch('./data/lessons_rows.json').then(r => r.json())
+  fetch('./data/lesson_rows.json').then(r => r.json())
 ]).then(([classes, schedules, teachers, lessons]) => {
 
   const teacherById = Object.fromEntries(teachers.map(t => [t.id, t]));
