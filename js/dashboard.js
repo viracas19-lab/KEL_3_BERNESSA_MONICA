7d94f95e3566e1194b6ef957ddf22a271f33abf7
viracas19-lab <viracas19@gmail.com>
2026-02-11 09:02:55 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,9 +11,9 @@ async function fetchDashboardData() {
     schedules
   ] = await Promise.all([
     fetch("data/teachers_rows.json").then(r => r.json()),
-    fetch("data/students_rows.json").then(r => r.json()),
-    fetch("data/classes_rows.json").then(r => r.json()),
-    fetch("data/lessons_rows.json").then(r => r.json()),
+    fetch("data/student_rows.json").then(r => r.json()),
+    fetch("data/class_rows.json").then(r => r.json()),
+    fetch("data/lesson_rows.json").then(r => r.json()),
     fetch("data/lesson_schedule_rows.json").then(r => r.json())
   ]);
 
@@ -62,3 +62,4 @@ async function initDashboard() {
 }
 
 initDashboard();
+
